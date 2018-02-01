25a4ad0b69ab4619d2cc62a23685f93cbea84140
Zacharias <32275536+ZachariasKing@users.noreply.github.com>
2018-02-01 13:23:47 +0000

    Update NameGenerator.java

--- NameGenerator.java
+++ NameGenerator.java
@@ -13,16 +13,16 @@ public class NameGenerator{
 	public static void nameGeneration()
 	{
 		System.out.print("Enter your first name: ");
-		Scanner input = new Scanner(System.in);
+		Scanner firstNameInput = new Scanner(System.in);
 		String fName =  input.nextLine();
 		System.out.print("Thank you, now enter your last name: ");
-		Scanner input2 = new Scanner(System.in);
+		Scanner lastNameInput = new Scanner(System.in);
 		String lName =  input.nextLine();
 		System.out.print("Thank you, now enter your mother's maiden name: ");
-		Scanner input3 = new Scanner(System.in);
+		Scanner maidenNameInput = new Scanner(System.in);
 		String maidenName =  input.nextLine();
 		System.out.print("Thank you, now enter your birth city: ");
-		Scanner input4 = new Scanner(System.in);
+		Scanner cityNameInput = new Scanner(System.in);
 		String cityName =  input.nextLine();
 		System.out.println("Your generated name is: " + lName.substring(0, 3) + fName.substring(0, 2) + " " + maidenName.substring(0, 2) + cityName.substring(0, 3));
 		
